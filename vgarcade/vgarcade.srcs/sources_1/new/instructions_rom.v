0ce5b47f5e2356910c8aa6dfcd7a0ebca7b70841
Jacob-Lemon <jrlemon03@gmail.com>
2024-07-03 17:12:48 +0100

    car should work now, added roms for game_state screens

---
+++ vgarcade/vgarcade.srcs/sources_1/new/instructions_rom.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/02/2024 01:56:13 PM
+// Design Name: 
+// Module Name: instructions_rom
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instructions_rom(
+
+    );
+endmodule
